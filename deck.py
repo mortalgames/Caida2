ab6d664535af1f1de5b97ee6df06cdee24929c8c
Francisco <andrewplanet@gmail.com>
2017-09-30 12:45:39 -0400

    Created new method and comment classes

--- deck.py
+++ deck.py
@@ -5,12 +5,30 @@ class Deck(object):
         self.cards = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12] * 4
         self.shuffle_deck()
     
-    def single_card(self):
-        index = self.cards.index(self.random_card())
-        return self.cards.pop(index)
+    def get_card(self):
+        """
+        Remove and return a single item from cards list
+        @return int
+        """
+        return self.cards.pop(self.__get_index_from_random_card())
+
+    def __get_index_from_random_card(self):
+        """
+        Get index from item in cards list
+        @return int
+        """
+        return self.cards.index(self.get_random_card())
         
-    def random_card(self):
+    def get_random_card(self):
+        """
+        Select a random card from cards list
+        @return int
+        """
         return random.choice(self.cards)
-        
+
     def shuffle_deck(self):
+        """ 
+        Shuffle cards list
+        @return list
+        """
         return random.shuffle(self.cards)
