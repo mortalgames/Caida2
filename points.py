e99eb346c4b2d274d00af513ce6b67eabdf9ca0d
Francisco <andrewplanet@gmail.com>
2017-09-29 23:11:12 -0400

    Points class

---
+++ points.py
@@ -0,0 +1,20 @@
+class Points(object):
+    @staticmethod
+    def one(self):
+        return 1
+        
+    @staticmethod
+    def two(self):
+        return 2
+        
+    @staticmethod        
+    def three(self):
+        return 3
+        
+    @staticmethod
+    def four(self):
+        return 4
+        
+    @staticmethod        
+    def clean(self):
+        return self.set_four()
