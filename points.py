9b7b4df8579b22b748b41dfc2865446c071ad446
Francisco <andrewplanet@gmail.com>
2017-09-30 12:46:04 -0400

    Comments for the class

--- points.py
+++ points.py
@@ -1,18 +1,35 @@
 class Points(object):
+
     @staticmethod
     def one():
+        """
+        Return one point
+        @return int
+        """
         return 1
         
     @staticmethod
     def two():
+        """
+        Return two point
+        @return int
+        """
         return 2
         
     @staticmethod        
     def three():
+        """
+        Return three point
+        @return int
+        """
         return 3
         
     @staticmethod
     def four():
+        """
+        Return four point
+        @return int
+        """
         return 4
         
     @staticmethod        
